583d178990d1229495d04995f9a30e8c74b4b4e4
Kenny Kurochkin <kennnkuro15@gmail.com>
2020-05-13 18:49:30 +0300

    fix

--- PetAdoptionC/NetworkManager.m
+++ PetAdoptionC/NetworkManager.m
@@ -1,8 +1,38 @@
 #import "NetworkManager.h"
 
 @implementation NetworkManager
-- (void)request:(NSString *)method {
-    NSLog(@"%@", method);
+- (void)request: (NSString *)method andURL: (NSString *)url {
+    
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setURL:[NSURL URLWithString: url]];
+    [request setHTTPMethod: method];
+    
+    NSURLSessionDataTask *task = [[self getURLSession] dataTaskWithRequest:request completionHandler:^( NSData *data, NSURLResponse *response, NSError *error )
+    {
+        dispatch_async( dispatch_get_main_queue(),
+        ^{
+            // parse returned data
+            NSString *result = [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
+            
+            NSLog( @"%@", result );
+        } );
+    }];
+    
+    [task resume];
+}
+
+
+- ( NSURLSession *)getURLSession
+{
+    static NSURLSession *session = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once( &onceToken,
+    ^{
+        NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+        session = [NSURLSession sessionWithConfiguration:configuration];
+    } );
+    
+    return session;
 }
 
 @end
